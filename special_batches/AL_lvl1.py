e493254482610ed1ea8911ca2b26dfa5e8576fad
Edward Smith Silvia <esmithsilvia@gmail.com>
2022-05-31 16:38:07 -0400

    assignment on list - lvl 1

--- special_batches/AL_lvl1.py
+++ special_batches/AL_lvl1.py
@@ -2,14 +2,16 @@
 
 def find_leap_years(given_year):
     list_of_leap_years=[]
-        
-    for count in range(1,16):
-        if(((given_year%4==0) and given_year%100!=0) or given_year%400!=0):
+    count = 0
+    while (count < 15):
+        if(((given_year%4==0) and given_year%100!=0) or given_year%400==0):
             list_of_leap_years.append(given_year)
             given_year += 4
+            count += 1
         else:
             given_year += 1
     return list_of_leap_years
 
 list_of_leap_years=find_leap_years(200)
 print(list_of_leap_years)
+
